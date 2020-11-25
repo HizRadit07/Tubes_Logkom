7eb455d02affcb6704a5508c6dce252ca9a3c0fb
16519413 M. Rafli Zamzami <kiipliwooke@gmail.com>
2020-11-25 07:27:45 +0700

    fixing inventory dan store

--- inventory.pl
+++ inventory.pl
@@ -7,24 +7,37 @@
 :- dynamic(armor/4).
 :- dynamic(accesories/4).
 
-bag([]).
-
 /*  weapon(ID,Nama,Class,Stat)
     armor(ID,Nama,Class,Stat)
     accesories(ID,Nama,Class,Stat)
 */
-id_item(1,'pedang').
-
-/*gachaWeaponSwordsman([pedang,pedanghitam,pedangbiru]).
-gachaWeaponArcher([panah,panahijo,panahmerah]).
-gachaWeaponSorcerer([staff,staffijo,staffcoklat]).
-gachaArmorSwordsman([kingarmor,redarmor,blackarmor]).
-gachaArmorArcher([silk,redsilk,bluesilk,darksilk]).
-gachaArmorSorcerer([robe,greatrobe]).
-gachaAccSwordsman([shield,bracelet,helmet]).
-gachaAccArcher([elvenquiver,griffonshoes,birdmask]).
-gachaAccSorcerer([manaring,bookofwisdom]).
-*/
+id_item(1,'Sword').
+id_item(2,'Black Sword').
+id_item(3,'Great Sword').
+id_item(4,'Bow').
+id_item(5,'Green Bow').
+id_item(6,'Long Bow').
+id_item(7,'Staff').
+id_item(8,'Staff of Apprentice').
+id_item(9,'Wizard Staff').
+id_item(10,'Armor').
+id_item(11,'Black Armor').
+id_item(12,'King Armor').
+id_item(13,'Silk').
+id_item(14,'Red Silk').
+id_item(15,'Dark Silk').
+id_item(16,'Dragon Silk').
+id_item(17,'Robe').
+id_item(18,'Great Robe').
+id_item(19,'Bracelet').
+id_item(20,'Helmet').
+id_item(21,'Elven Quiver').
+id_item(22,'Griffon Shoes').
+id_item(23,'Bird Mask').
+id_item(24,'Book of Apprentice').
+id_item(25,'Mana Ring').
+id_item(26,'Book of Wisdom').
+id_item(27,'Slytherin Cloak').
 
 item_storage(0).
 total_potions(0).
@@ -71,24 +84,31 @@ consume_attpotions :-
     retract(total_attpotions(M)),
     assertz(total_attpotions(M1)).
 
-addS(X,[],[X]): true,!.
-addS(X,[H|T],[H|T1]):-
-    addS(X,T,T1).
 
+bag([]).
+
+konsdot(X,[],[X]):-!.
+konsdot(X,[H|T],[H|T1]):-
+    konsdot(X,T,T1),!.
+
+konso(X,[],[X]).
+konso(X,Y,[X|Y]):- !.
+
+remove(X,[X],[]).
 remove(X,[X|Xs],Xs):- !.
-remove(X,[_|Xs],Result):-
+remove(X,[Y|Xs],Result):-
     remove(X,Xs,Tmp),
-    addS(X,Tmp,Result).
+    konso(Y,Tmp,Result),!.
 
 add_weapon(Name,Class,Stats):-
     item_storage(N),
     N1 is N+1,
     retract(item_storage(N)),
     assertz(item_storage(N1)),
-    id_item(ID,Name),
-    assertz(weapon(ID,Name,Class,Stats)).
+    id_item(ID,Name),!,
+    assertz(weapon(ID,Name,Class,Stats)),
     bag(Bag),
-    addS(ID,Bag,Resbag),
+    konsdot(ID,Bag,Resbag),
     retract(bag(Bag)),
     assertz(bag(Resbag)).
 
@@ -97,10 +117,10 @@ add_armor(Name,Class,Stats):-
     N1 is N+1,
     retract(item_storage(N)),
     assertz(item_storage(N1)),
-    id_item(ID,Name),
-    assertz(armor(ID,Name,Class,Stats)).
+    id_item(ID,Name),!,
+    assertz(armor(ID,Name,Class,Stats)),
     bag(Bag),
-    addS(ID,Bag,Resbag),
+    konsdot(ID,Bag,Resbag),
     retract(bag(Bag)),
     assertz(bag(Resbag)).
 
@@ -109,10 +129,10 @@ add_accesories(Name,Class,Stats):-
     N1 is N+1,
     retract(item_storage(N)),
     assertz(item_storage(N1)),
-    id_item(ID,Name),
-    assertz(accesories(ID,Name,Class,Stats)).
+    id_item(ID,Name),!,
+    assertz(accesories(ID,Name,Class,Stats)),
     bag(Bag),
-    addS(ID,Bag,Resbag),
+    konsdot(ID,Bag,Resbag),
     retract(bag(Bag)),
     assertz(bag(Resbag)).
 
@@ -123,7 +143,7 @@ remove_weapon(ID):-
     assertz(item_storage(N1)),
     retract(weapon(ID,_,_,_)),
     bag(Bag),
-    remove(ID,Bag,Resbag),
+    remove(ID,Bag,Resbag),!,
     retract(bag(Bag)),
     assertz(bag(Resbag)).
 
@@ -134,7 +154,7 @@ remove_armor(ID):-
     assertz(item_storage(N1)),
     retract(armor(ID,_,_,_)),
     bag(Bag),
-    remove(ID,Bag,Resbag),
+    remove(ID,Bag,Resbag),!,
     retract(bag(Bag)),
     assertz(bag(Resbag)).
 
@@ -145,15 +165,15 @@ remove_accesories(ID):-
     assertz(item_storage(N1)),
     retract(accesories(ID,_,_,_)),
     bag(Bag),
-    remove(ID,Bag,Resbag),
+    remove(ID,Bag,Resbag),!,
     retract(bag(Bag)),
     assertz(bag(Resbag)).
 
 show_bag([]).
-show_bag([H|T]);-
+show_bag([H|T]):-
     id_item(H,Nama),
     (weapon(H,Nama,Class,_),!;armor(H,Nama,Class,_),!;accesories(H,Nama,Class,_),!),
-    write(Nama),write(' ('),write(Class),write(')'),nl,
+    write('1 '),write(Nama),write(' ('),write(Class),write(').'),nl,
     show_bag(T),!.
 
 inventory:-
--- store.pl
+++ store.pl
@@ -1,83 +1,137 @@
 /* Predikat yang bisa aja masuk ke dalam inventory */
+:- dynamic(generate_number/1).
+
+generate_number(53).
+
+
 
 potion:-
-    add_potions(1),
     character_gold(X),
-    X1 is X-20,
-    retract(character_gold(X)),
-    assertz(character_gold(X1)).
+    (
+        ((X<10)->
+            write('Your Gold is not enough to buy a Potion.'),nl
+        );
+        (
+            add_potions(1),
+            X1 is X-10,
+            retract(character_gold(X)),
+            assertz(character_gold(X1)),
+            total_potions(N),
+            write('You have '),write(N),write(' Potion(s).'),nl,
+            write('Your gold remaining '),write(X1),nl
+        )
+    ).
+
+attackPotion:-
+    character_gold(X),
+    (
+        ((X<15)->
+            write('Your Gold is not enough to buy an Attaack Potion.'),nl
+        );
+        (
+            add_attpotions(1),
+            X1 is X-15,
+            retract(character_gold(X)),
+            assertz(character_gold(X1)),
+            total_attpotions(N),
+            write('You have '),write(N),write(' Attack Potion(s).'),nl,
+            write('Your gold remaining '),write(X1),nl
+        )
+    ).
 
 exitShop:-
-    write('Thanks for coming').
+    write('Thanks for coming.').
 
+gachaItem(1,'Sword','weapon','Swordsman',23).
+gachaItem(2,'Black Sword','weapon','Swordsman',25).
+gachaItem(3,'Great Sword','weapon','Swordsman',27).
+gachaItem(4,'Bow','weapon','Archer',23).
+gachaItem(5,'Green Bow','weapon','Archer',25).
+gachaItem(6,'Long Bow','weapon','Archer',27).
+gachaItem(7,'Staff','weapon','Sorcerer',23).
+gachaItem(8,'Staff of Apprentice','weapon','Sorcerer',25).
+gachaItem(9,'Wizard Staff','weapon','Sorcerer',27).
+gachaItem(10,'Armor','armor','Swordsman',13).
+gachaItem(11,'Black Armor','armor','Swordsman',15).
+gachaItem(12,'King Armor','armor','Swordsman',17).
+gachaItem(13,'Silk','armor','Archer',13).
+gachaItem(14,'Red Silk','armor','Archer',14).
+gachaItem(15,'Dark Silk','armor','Archer',15).
+gachaItem(16,'Dragon Silk','armor','Archer',17).
+gachaItem(17,'Robe','armor','Sorcerer',14).
+gachaItem(18,'Great Robe','armor','Sorcerer',16).
+gachaItem(19,'Bracelet','accesories','Swordsman',20).
+gachaItem(20,'Helmet','accesories','Swordsman',40).
+gachaItem(21,'Elven Quiver','accesories','Archer',19).
+gachaItem(22,'Griffon Shoes','accesories','Archer',29).
+gachaItem(23,'Bird Mask','accesories','Archer',40).
+gachaItem(24,'Book of Apprentice','accesories','Sorcerer',15).
+gachaItem(25,'Mana Ring','accesories','Sorcerer',24).
+gachaItem(26,'Book of Wisdom','accesories','Sorcerer',34).
+gachaItem(27,'Slytherin Cloak','accesories','Sorcerer',45).
 
-gachaWeaponSwordsman([pedang,pedanghitam,pedangbiru]).
-gachaWeaponArcher([panah,panahijo,panahmerah]).
-gachaWeaponSorcerer([staff,staffijo,staffcoklat]).
-gachaArmorSwordsman([kingarmor,redarmor,blackarmor]).
-gachaArmorArcher([silk,redsilk,bluesilk,darksilk]).
-gachaArmorSorcerer([robe,greatrobe]).
-gachaAccSwordsman([shield,bracelet,helmet]).
-gachaAccArcher([elvenquiver,griffonshoes,birdmask]).
-gachaAccSorcerer([manaring,bookofwisdom]).
+total_Gacha_Item(27).
 
-length([],Y):- Y is 0.
-length([X|Xs],Y):-
-    length(Xs,M),
+in_shop(false).
+
+/*Bekas gunain list*/
+/*len([],Y):- Y is 0.
+len([_|Xs],Y):-
+    len(Xs,M),
     Y is M+1.
 
 front([X|_],Y):- Y is X,!.
 
 pop([_|Xs],Y):- Y = Xs,!.
 
-elemt(1,ListA,Result):-
-    front(ListA,Head),
+elemt(1,_A,Result):-
+    front(_A,Head),
     Result is Head,!.
-elemt(X,List,Result):-
-    pop(List,TailS),
+elemt(X,_A,Result):-
+    pop(_A,TailS),
     X1 is X-1,
-    elemt(X1,TailS,Result).
+    !,elemt(X1,TailS,Result).
+*/
+
 gacha:-
 /*  jadi gacha ini teknisnya dia bakal ngerandom list possible itemnya
     terus ntar keluarin headnya gitu*/
-    random(1,10,Tipe),
+    character_gold(Gold),
     (
-    ((Tipe==1)->
-        gachaWeaponSwordsman(Item_Gacha),
-        length(Item_Gacha,TotalItem)
-    );
-    ((Tipe==2)->
-        gachaWeaponArcher(Item_Gacha),
-        length(Item_Gacha,TotalItem)
-    );
-    ((Tipe==3)->
-        gachaWeaponSorcerer(Item_Gacha),
-        length(Item_Gacha,TotalItem)
-    );
-    ((Tipe==4)->
-        gachaArmorSwordsman(Item_Gacha),
-        length(Item_Gacha,TotalItem)
-    );
-    ((Tipe==5)->
-        gachaArmorArcher(Item_Gacha),
-        length(Item_Gacha,TotalItem)
-    );
-    ((Tipe==6)->
-        gachaArmorSorcerer(Item_Gacha),
-        length(Item_Gacha,TotalItem)
-    );
-    ((Tipe==7)->
-        gachaAccSwordsman(Item_Gacha),
-        length(Item_Gacha,TotalItem)
-    );
-    ((Tipe==8)->
-        gachaArmorArcher(Item_Gacha),
-        length(Item_Gacha,TotalItem)
-    );
-    ((Tipe==9)->
-        gachaAccSorcerer(Item_Gacha),
-        length(Item_Gacha,TotalItem)
-    )
-    ),
-    Rightboundary is TotalItem+1,
-    random(1,Rightboundary,GetIdx).
+        ((Gold<100)->
+            write('Your Gold is not enough to buy a gacha.'),nl
+        );
+        (
+            Gold1 is Gold-100,
+            retract(character_gold(Gold)),
+            assertz(character_gold(Gold1)),
+            total_Gacha_Item(Divisor),
+            generate_number(M),
+            Upperbound is M+1,
+            retract(generate_number(M)),
+            assertz(generate_number(Upperbound)),
+            random(1,Upperbound,IdxRaw),
+            IdxRaw1 is IdxRaw mod Divisor,
+            Random_Number is IdxRaw1+1,
+            gachaItem(Random_Number,Name,Type,Class,Stats),
+            write('You get '),write(Name),write(' (+'),write(Stats),
+            (
+                ((Type=='weapon')->
+                    write(' attack'),
+                    add_weapon(Name,Class,Stats)
+                );
+                ((Type=='armor')->
+                    write(' defense'),
+                    add_armor(Name,Class,Stats)
+                );
+                ((Type=='accesories')->
+                    write(' HP'),
+                    add_accesories(Name,Class,Stats)
+                )
+            ),
+            write(')'),nl,
+            write('You have '),write(Gold1),write(' gold remaining.'),nl
+        )
+    ).
+
+    
