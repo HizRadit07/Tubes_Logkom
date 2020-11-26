df539647eaec5a4ebf973c6e2612cc91d3f64ea4
pranagusriana <pranagusriana@gmail.com>
2020-11-27 02:18:10 +0700

    udah bisa battle, tinggal penyesuaian damage sama tambahan-tambahan?

--- battle.pl
+++ battle.pl
@@ -1,14 +1,21 @@
 :- dynamic(usedSpecialatk/2).
 :- dynamic(turn/1).
 :- dynamic(gameState/1).
+:- dynamic(turnspecial/2).
 
-/* game state nya itu baru kepikiran start, meetEnemy, fight */
+/* game state : start, meetEnemy, fight */
 /* meetEnemy itu pas ketemu enemy, ada 2 pilihan command fight sama run */
-/* fight itu pas gagal run atau emang milih fight ada pilihan attack, specialAttack, usePotion, run */
+/* fight itu pas gagal run atau emang milih fight ada pilihan attack, special_attack, usePotion, run */
 /* use atk potion belum */
+
 turn(-1).
 gameState(notStart).
 usedSpecialatk(-1,enemy).
+turnspecial(0, 0).
+
+modulo2(X,R) :-
+    R is X mod 2.
+
 /* Buat set GameState nya, baru kepikiran start, meetEnemy, fight */
 setGameState(State) :-
     retract(gameState(_)),
@@ -18,6 +25,8 @@ setGameState(State) :-
 startBattle :-
     retract(turn(_)),
     assertz(turn(0)),
+    retract(turnspecial(_, _)),
+    assertz(turnspecial(0, 0)),
     retract(usedSpecialatk(_, enemy)),
     assertz(usedSpecialatk(0, enemy)), !.
 
@@ -25,57 +34,94 @@ startBattle :-
 run :-
     gameState(meetEnemy),
     random(0, 11, X),
-    ((X < 5) -> fight, write('Sorry, You failed to run!'), nl ;
-    map, write('You run from battle'), setGameState(start)), nl, !.
+    (X < 5 -> write('Sorry, You failed to run!'), nl, fight ;
+    write('You run from battle'), nl, setGameState(start), map, nl), !.
 
 run :-
     gameState(fight),
     random(0, 11, X),
-    ((X < 5) -> fight, write('Sorry, You failed to run!'), nl, nextTurn, battle ;
-    map, write('You run from battle'), setGameState(start)), nl, !.
+    (X < 5 -> write('Sorry, You failed to run!'), nl, nextTurn, nextturnspecialatk, battle ;
+    write('You run from battle'), nl, setGameState(start), map, nl), !.
 
 /* Attack, specialAttack */
+
 attack :-
     gameState(fight),
     current_enemy_stat(_,Classenemy,_,_,_,_),
-    current_enemy(HP_enemy, Atk_enemy, Def_enemy),
-    character_status(Player, HP_player, Atk_player, Def_player),
+    current_enemy(_, _, Def_enemy),
+    character_status(Id, _, Atk_player,_),
+    current_class(Id, _),
     write('Attacking enemy'),
     nl,
     /* mekanisme yang equip weapon belom dimasukkin */
     Damage is Atk_player - Def_enemy,
-    ((Damage < 0) -> (Total_dmg is 0, Newdef_enemy is Def_enemy-Atk_player));((Damage >= 0) -> (Total_dmg is Damage, Newdef_enemy is 0)),
+    (Damage < 0 -> Total_dmg is 0, Newdef_enemy is Def_enemy-1/4*Atk_player;
+     Damage >= 0 -> Total_dmg is Damage, Newdef_enemy is Def_enemy-1/4*Damage),
     write(Classenemy), write(' took '), write(Total_dmg), write(' damage'),
     nl,
     damage_enemy(Total_dmg),
     set_def_enemy(Newdef_enemy),
+    turnspecial(X, _),
+    NewX is X+1,
+    retract(turnspecial(_,_)),
+    assertz(turnspecial(NewX, _)),
     cekStatus, !.
 
 attack :-
     gameState(_),
     write('This command can only used in battle'), nl,!.
 
+nextturnspecialatk :-
+    turnspecial(X, _),
+    NewX is X+1,
+    retract(turnspecial(_,_)),
+    assertz(turnspecial(NewX, _)).
+
+
+cekSpecialatt(R) :-
+    turnspecial(X,_),
+    X>=3,
+    retract(turnspecial(_,_)),
+    assertz(turnspecial(X, 1)),
+    R is 1, !.
+
+cekSpecialatt(R) :-
+    turnspecial(X,_),
+    X<3,
+    R is 0, !.
+
+resetturnspecial :-
+    retract(turnspecial(_,_)),
+    assertz(turnspecial(0, 0)).
+
 special_attack :-
     gameState(fight),
-    turn(X), (X+2)/2 mod 3 == 0,
+    cekSpecialatt(R),
+    R is 1,
     current_enemy_stat(_,Classenemy,_,_,_,_),
-    current_enemy(HP_enemy, Atk_enemy, Def_enemy),
-    character_status(Player, HP_player, Atk_player, Def_player),
-    Atk_player1 is 2.5 * Atk_player,
+    current_enemy(_, _, Def_enemy),
+    character_status(Id, _, Atk_player, _),
+    current_class(Id, _),
+    Atk_player1 is 2 * Atk_player,
     write('Attacking enemy'),
     nl,
     Damage is Atk_player1 - Def_enemy,
-    ((Damage < 0) -> (Total_dmg is 0, Newdef_enemy is Def_enemy-Atk_player1));((Damage >= 0) -> (Total_dmg is Damage, Newdef_enemy is 0)),
+    (Damage < 0 -> Total_dmg is 0, Newdef_enemy is Def_enemy-1/4*Atk_player1;
+     Damage >= 0 -> Total_dmg is Damage, Newdef_enemy is Def_enemy-1/4*Damage),
     write(Classenemy), write(' took '), write(Total_dmg), write(' damage'),
     nl,
     damage_enemy(Total_dmg),
     set_def_enemy(Newdef_enemy),
+    resetturnspecial,
     cekStatus, !.
 
 special_attack :-
     gameState(fight),
-    turn(X), Z is (X+2)/2 mod 3, Z =\= 0,
-    write('You can use special attack '), write(Z), write(' turn left').
+    cekSpecialatt(R),
+    R is 0,
+    turnspecial(X,0), 
+    Z is 3-X,
+    write('You can use special attack '), write(Z), write(' turn left'), nl, !.
 
 special_attack :-
     gameState(_),
@@ -83,27 +129,29 @@ special_attack :-
 
 /* Enemy attack */
 enemyAttack :-
-    current_enemy_stat(_,Classenemy,_,_,_,_),
-    current_enemy(HP_enemy, Atk_enemy, Def_enemy),
-    character_status(Player, HP_player, Atk_player, Def_player),
-    (((HP_player < 50) -> Atk_enemy1 is 2*Atk_enemy);
-    (Atk_enemy1 is Atk_player)),
+    current_enemy_stat(_,_,_,_,_,_),
+    current_enemy(_, Atk_enemy, _),
+    character_status(Id, HP_player, Atk_player, Def_player),
+    current_class(Id, Player),
+    (HP_player < 50 -> Atk_enemy1 is 2*Atk_enemy;
+     Atk_enemy1 is Atk_player),
     Damage is Atk_enemy1 - Def_player,
-    ((Damage < 0) -> (Total_dmg is 0, Newdef_player is Def_player-Atk_enemy1));((Damage >= 0) -> (Total_dmg is Damage, Newdef_player is 0)),
+    (Damage < 0 -> Total_dmg is 0, Newdef_player is Def_player-Atk_enemy1;
+     Damage >= 0 -> Total_dmg is Damage, Newdef_player is 0),
     write(Player), write(' took '), write(Total_dmg), write(' damage'),
     nl,
-    NewHP is HP-Total_dmg,
+    NewHP is HP_player-Total_dmg,
     set_char_hp(NewHP),
     set_char_def(Newdef_player),
     cekStatus, !.
-
+    
 /* enemy & player turn */
 enemyTurn :-
     write('Enemy attack'), nl,
     enemyAttack, !.
 
 playerTurn :-
-    write('Type "attack", "specialAttack", "run", or "usePotion"'), nl,
+    write('Type "attack", "special_attack", "run", or "usePotion"'), nl,
     hpStat,
     !.
 
@@ -116,7 +164,7 @@ failState :-
 winBattle :-
     write('You won the battle'), nl,
     current_enemy_stat(_,Classenemy,_,_,_,_),
-    current_enemy(HP_enemy, Atk_enemy, Def_enemy),
+    current_enemy(_, _, _),
     record_kill(Classenemy),
     check_quest,
     setGameState(start).
@@ -131,19 +179,22 @@ nextTurn :-
 
 battle :-
     turn(X),
-    ((X mod 2 == 0) -> playerTurn; (X mod 2 == 1) -> enemyTurn), !.
+    modulo2(X, R),
+    (R == 0 -> playerTurn;
+     R == 1 -> enemyTurn), !.
 
 cekStatus :-
     current_enemy(HP_enemy,_,_),
     character_status(_,HP_player,_,_),
-
-    ((HP_player =< 0 -> failState);
-    (HP_enemy =< 0 -> winBattle; nextTurn, battle)), !.
+    
+    (HP_player =< 0 -> failState;
+     HP_enemy =< 0 -> winBattle; nextTurn, battle), !.
 
 hpStat :-
     current_enemy_stat(_,Classenemy,_,_,_,_),
     current_enemy(HP_enemy,_,_),
-    character_status(Player, Hp_player,_,_),
+    character_status(Id, Hp_player,_,_),
+    current_class(Id, Player),
     write('HP '), write(Player), write(' : '), write(Hp_player),
     nl,
     write('HP '), write(Classenemy), write(' : '), write(HP_enemy), !.
@@ -158,8 +209,14 @@ fight :-
 usePotion :-
     gameState(fight),
     total_potions(N),
-    current_enemy_stat(_,Classenemy,_,_,_,_),
-    current_enemy(HP_enemy, Atk_enemy, Def_enemy),
+    /*current_enemy_stat(_,Classenemy,_,_,_,_),
+    current_enemy(HP_enemy, Atk_enemy, Def_enemy),*/
     character_status(_,HP_player,_,_),
     base_stat(BaseHP,_,_),
-    ((N =\= 0) -> ((((HP_player+100 =< BaseHP) -> NewHP is HP_player+100); NewHP is BaseHP), write('You heal 100 HP'), nl, set_char_hp(NewHP), consume_potion, nextTurn, battle);write('You dont have enough potion'), nl, battle).
+    (N =\= 0 -> 
+        (HP_player+100 =< BaseHP -> NewHP is HP_player+100; NewHP is BaseHP), write('You heal 100 HP'), nl,
+         set_char_hp(NewHP), consume_potion, nextTurn, battle;
+         write('You dont have enough potion'), nl, battle).
+    
+    
+
